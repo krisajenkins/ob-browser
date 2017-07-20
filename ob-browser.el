a347d9df1c87b7eb660be8723982c7ad2563631a
Kris Jenkins <krisajenkins@gmail.com>
2017-07-20 20:18:22 +0100

    Adding a license header.

--- ob-browser.el
+++ ob-browser.el
@@ -1,6 +1,7 @@
 ;;; ob-browser.el --- Render HTML in org-mode blocks.
 ;; Copyright 2013 Kris Jenkins
 
+;; License: GNU General Public License version 3, or (at your option) any later version
 ;; Author: Kris Jenkins <krisajenkins@gmail.com>
 ;; Maintainer: Kris Jenkins <krisajenkins@gmail.com>
 ;; Keywords: org babel browser phantomjs
